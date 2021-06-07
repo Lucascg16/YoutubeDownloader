7f63993cf04e862583c91eea7e014f4ed209f736
Lucas Carlos Giacomin <73847333+Lucascg16@users.noreply.github.com>
2021-06-07 20:31:30 -0300

    Update YoutubeDown.py

--- YoutubeDown.py
+++ YoutubeDown.py
@@ -1,4 +1,6 @@
 from pytube import YouTube
+print('Bem-Vindo ao YoutubeDownloader, insira o link do video que deseja baixar.')
+print('Ele irá aparecer na pasta onde o script esta localizado.')
 link = input("Video Link:")
 video = YouTube(link)
 stream = video.streams.get_highest_resolution()
